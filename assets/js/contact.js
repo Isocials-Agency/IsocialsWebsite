f5be7313acace7c1d9946b6dc75dcd9f524b55ac
Othman <othmanmaher@dimensions-studio.com>
2024-06-06 00:14:55 +0300

    finishing the website

---
+++ assets/js/contact.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("contact");
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const formData = new FormData(form);
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+
+    fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: json,
+    })
+      .then(function () {
+        form.reset();
+      });
+  });
+});
